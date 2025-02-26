fb004c4b8558dce992cb9bb8d476bad78c7e0723
mehulbhanvadiya <csmehul22@gmail.com>
2025-02-26 20:51:22 +0530

    Updates

---
+++ precache-manifest.d58014d26b155ad394f03d492d0ba558.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2ce54965ade918c22975ffef6db3c8",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "68363e19c424191e5f4c",
+    "url": "/resume/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b89c6f50bbd68c1ade33",
+    "url": "/resume/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "68363e19c424191e5f4c",
+    "url": "/resume/static/js/2.098acdc1.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/resume/static/js/2.098acdc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89c6f50bbd68c1ade33",
+    "url": "/resume/static/js/main.242dda3d.chunk.js"
+  },
+  {
+    "revision": "76f5f8cabe4dd694054a",
+    "url": "/resume/static/js/runtime-main.a8641cc8.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/resume/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/resume/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/resume/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/resume/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/resume/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/resume/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/resume/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/resume/static/media/software.43049cae.jpeg"
+  }
+]);
